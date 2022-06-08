1d81366c5a6aa3a2a1db5040192bf5c403f4bcdc
Aubrey Selamu-Bell <aubrey.selamu@Victorias-Air.localdomain>
2022-06-08 13:06:54 -0400

    updated Users model

--- models/user.js
+++ models/user.js
@@ -20,7 +20,6 @@ class User {
 
     let hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR)
     const results = await db.query(`
-
           INSERT INTO users (username, password, first_name, last_name, phone, join_at, last_login_at)
           VALUES ($1, $2, $3, $4, $5, current_timestamp, current_timestamp)
           RETURNING username, password, first_name, last_name, phone`,
@@ -32,13 +31,33 @@ class User {
 
   /** Authenticate: is this username/password valid? Returns boolean. */
 
-  static async authenticate(username, password) { 
+  static async authenticate(username, password) {
+    const result = await db.query(`
+          SELECT password
+          FROM users
+          WHERE username = $1`, 
+          [username]
+          )
     
+    let user = result.rows[0]
+    return user && bcrypt.compare(password, user.password)
   }
 
   /** Update last_login_at for user */
 
-  static async updateLoginTimestamp(username) { }
+  static async updateLoginTimestamp(username) { 
+    const result = await db.query(`
+          UPDATE users
+          SET last_login_at = current_timestamp
+          WHERE username = $1
+          RETURNING username`,
+          [username]
+          );
+    
+    if(!result.rows[0]){
+      throw new ExpressError(`No such user ${username}`, 404)
+    }
+  }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name, phone}, ...] */
@@ -83,7 +102,27 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesFrom(username) { }
+  static async messagesFrom(username) { 
+    const result = await db.query(`
+          SELECT m.id, m.to_username, u.first_name, u.last_name, u.phone, m.body, m.sent_at, m.read_at
+          FROM messages AS m
+          JOIN users AS u ON m.to_username = u.username
+          WHERE from_username = $1`,
+          [username])
+
+    return result.rows.map(m => ({
+          id: m.id,
+          to_user: {
+            username: m.to_username,
+            first_name: m.first_name,
+            last_name: m.last_name,
+            phone: m.phone
+          },
+          body: m.body,
+          sent_at: m.sent_at,
+          read_at: m.read_at
+    }))
+  }
 
   /** Return messages to this user.
    *
@@ -93,7 +132,29 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesTo(username) { }
+  static async messagesTo(username) { 
+      const result = await db.query(`
+            SELECT m.id, m.from_username, u.first_name, u.last_name, u.phone, m.body, m.sent_at, m.read_at
+            FROM messages AS m
+            JOIN users AS u ON m.from_username = u.username
+            WHERE m.to_username = $1`,
+            [username]
+            );
+
+      return result.rows.map(m => ({
+          id: m.id,
+          from_user: {
+            username: m.from_username,
+            first_name: m.first_name,
+            last_name: m.last_name,
+            phone: m.phone
+          },
+          body: m.body,
+          sent_at: m.sent_at,
+          read_at: m.read_at
+      }))
+
+  }
 }
 
 
