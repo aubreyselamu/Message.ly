2f3ab222efee885c1eb650b309d91fb4753c59c0
Aubrey Selamu-Bell <aubrey.selamu@Aubreys-MBP.lan>
2022-06-10 11:04:48 -0400

    implemented routes for getting users and a user

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,42 @@
+const User = require("../models/user")
+
+
+const Router = require("express").Router
+const router = new Router()
+
+
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
 
+router.get("/", async (req, res, next) => {
+    try{
+        let users = await User.all();
+        return res.json(users)
+    }
+    catch(err){
+        return next(err)
+    }
+})
 
 /** GET /:username - get detail of users.
  *
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
+
+router.get('/:username', async(req, res, next) => {
+    try{
+        let user = await User.get(req.params.username)
+        return res.json(user)
+    }
+
+    catch(err){
+        return next(err)
+    }
+})
 
 
 /** GET /:username/to - get messages to user
